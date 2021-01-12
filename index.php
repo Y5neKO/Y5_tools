40e6af3699253e1d24b9a4308c4d0448f08adbdd
Y5neKO <41314905+Y5neKO@users.noreply.github.com>
2021-01-12 08:43:27 +0800

    主引导页

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>Y5neKO的在线工具</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/style.css">
+        <style>
+            #container {
+                width: 90%;
+                overflow:auto; 
+                margin: 0 auto;
+            }
+        </style>
+    </head>
+    
+    <body id="container">
+        <center>
+            <h1 id="container">Y5neKO's在线工具</h1>
+            <br>
+            <a href="./CyberChef"><button class="btn-1">CyberChef-高级编码分析工具</button></a>
+            <a href="./bin_QR"><button class="btn-2">二进制转二维码</button></a>
+            <a href="./edcode"><button class="btn-3">在线加解密</button></a>
+            <a href="./bacon"><button class="btn-4">培根密码(BUG)</button></a>
+            <a href="./brainfuck"><button class="btn-5">brainfuck/Ook!加解密</button></a>
+            <a href="./jsfuck"><button class="btn-1">jsfuck加解密</button></a>
+            <a href="./base64_image"><button class="btn-2">base64转图片</button></a>
+            <br>
+            <br>
+            <a>待添加</a><br>
+            <a>可以联系我投稿在线工具 1727058834@qq.com</a><br>
+        </center>
+    </body>
+</html>
